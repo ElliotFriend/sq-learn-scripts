f860fa3872ef75a94de585348d8492eb70dc385e
Elliot Voris <elliot@voris.me>
2022-08-17 10:37:09 -0500

    Breaking the first sql quest snippet
    Signed-off-by: Elliot Voris <elliot@voris.me>

--- broken/sql01/sql0101.js
+++ broken/sql01/sql0101.js
@@ -1,53 +1,36 @@
-(async () => {
-  const {
-    Keypair,
-    Server,
-    TransactionBuilder,
-    Networks,
-    Operation,
-    BASE_FEE
-  } = require('stellar-sdk')
-  const { friendbot } = require('../../sq-learn-utils')
+const {
+  Keypair,
+  Server,
+  TransactionBuilder,
+  Networks,
+  Operation,
+  BASE_FEE
+} = require('stellar-sdk')
 
-  // Part 1: Create the Quest Account by funding the Quest Keypair with XLM from friendbot
+const questKeypair = Keypair.fromSecret('SECRET_KEY_HERE');
+const newKeypair = null // create a new keypair here
 
-  // const questKeypair = Keypair.fromSecret('SECRET_KEY_HERE');
-  const questKeypair = Keypair.random()
+const server = null // create your server here
+const questAccount = null // load your quest account here
 
-  // Optional: Log the keypair details if you want to save the information for later.
-  console.log(`Quest Public Key: ${questKeypair.publicKey()}`)
-  console.log(`Quest Secret Key: ${questKeypair.secret()}`)
+const transaction = new TransactionBuilder(
+  questAccount, {
+    fee: BASE_FEE,
+    networkPassphrase: Networks.TESTNET
+  })
+  /* Complete your transaction below this line
+   * add your `createAccount` operation, set a timeout, and don't forget to build()
+   */
 
-  await friendbot(questKeypair.publicKey())
 
-  // Part 2: Create a new account using the `createAccount` operation with the Quest Account as the source account
 
-  const server = new Server('https://horizon-testnet.stellar.org')
-  const questAccount = await server.loadAccount(questKeypair.publicKey())
-  const newKeypair = Keypair.random()
+  /* Complete your transaction above this line */
 
-  // Optional: Log the keypair details if you want to save the information for later.
-  console.log(`Created Public Key: ${newKeypair.publicKey()}`)
-  console.log(`Created Secret Key: ${newKeypair.secret()}`)
+// sign your transaction here
 
-  const transaction = new TransactionBuilder(
-    questAccount, {
-      fee: BASE_FEE,
-      networkPassphrase: Networks.TESTNET
-    })
-    .addOperation(Operation.createAccount({
-      destination: newKeypair.publicKey(),
-      startingBalance: '1000'
-    }))
-    .setTimeout(30)
-    .build()
-
-  transaction.sign(questKeypair)
-
-  try {
-    const res = await server.submitTransaction(transaction)
-    console.log(`Transaction Successful! Hash: ${res.hash}`)
-  } catch (error) {
-    console.log(`${error}: More details:\n${error.response.data}`)
-  }
-})()
+try {
+  const res = null // submit your transaction here
+  console.log(`Transaction Successful! Hash: ${res.hash}`)
+} catch (error) {
+  console.log(`${error}: More details:\n${error.response.data}`)
+}
